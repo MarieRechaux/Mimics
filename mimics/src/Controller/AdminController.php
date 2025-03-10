2f468bc3c25fa57e5e794a76a4ec0ffc7d6cebbd
28010-59-15 <28010-59-15@exchange.ad.afpanet>
2025-03-10 16:53:37 +0100

    seconde version Mimics

--- mimics/src/Controller/AdminController.php
+++ mimics/src/Controller/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use PDO;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class AdminController extends AbstractController
 {
@@ -35,4 +41,88 @@ final class AdminController extends AbstractController
         return $this->render('admin/users.html.twig', [
         ]);
     }
+
+    #[Route('/admin/category', name: 'app_admin_category')]
+    public function adminCategory(Request $request, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): Response
+    {
+        $category = new Category;
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $category->setCreatedAt(new \DateTimeImmutable());
+
+            // $connect_db->prepare("INSERT INTO category VALUES (:title)")
+            // $connect_db->bindValue(':title', $category->getTitle(), PDO::PARAM_STR)
+            $entityManager->persist($category);
+
+            // $stmt->execute
+            $entityManager->flush();
+
+            // Message utilisateur stockés en session
+            $this->addFlash('success', 'La categorie a été enregistrée.');
+
+            return $this->redirectToRoute('app_admin_category');
+        }
+
+        /*
+            Une classe Repository contient des méthodes permettant uniquement d'executer des reuêtes de sélections (SELECT) EN BDD (find($id), findAll(), findBy(), findOneBy())
+        */
+
+        $dbCategory = $repoCategory->findAll();
+        // dump($dbCategory);
+
+        return $this->render('admin/category.html.twig', [
+            'categoryForm' => $form,
+            'dbCategory' => $dbCategory
+        ]);
+    }
+
+    #[Route('/admin/category/update/{id}', name: 'app_admin_category_update')]
+    public function adminCategoryUpdate($id, Category $category, Request $request, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): response
+    {
+        // dump($category);
+
+        // SELECT * FROM category WHERE id = $id;
+        // + fetch(PDO::FETCH_ASSOC)
+        $category = $repoCategory->find($id);
+        // dump($id);
+        // dump($category);
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // UPDATE category SET title = $category->geTitle(), description = $category->getDescription WHERE id = $id
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            dump($category->getTitle());
+            $categoryTitle = $category->getTitle();
+
+            $this->addFlash('success', "La catégorie <strong class='text-white'>$categoryTitle</strong> a été modifier");
+
+            return $this->redirectToRoute('app_admin_category');
+        }
+
+        $dbCategory = $repoCategory->findAll();
+
+        return $this->render('admin/category.html.twig', [
+            'categoryForm' => $form,
+            'dbCategory' => $dbCategory
+        ]);
+    }
+
+    #[Route('/admin/category/remove/{id}', name: 'app_admin_category_remove')]
+    public function adminCategoryRemove($id, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): response
+    {
+        return $this->render('admin/users.html.twig', [
+        ]);
+    }
+
 }
